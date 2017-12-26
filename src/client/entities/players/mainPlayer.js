82143aa2ba18224f29fefc73844ddf18e1447116
Benjamin J Anderson <banderson@mashable.com>
2017-12-26 17:27:02 -0600

    add direction standing

--- src/client/entities/players/mainPlayer.js
+++ src/client/entities/players/mainPlayer.js
@@ -27,8 +27,14 @@ export default class MainPlayer extends me.ComposableSpritePlugin.ComposableSpri
     this.renderable.addAnimation('walk_left', [117,118,119,120,121,122,123,124,125], 150);
     this.renderable.addAnimation('walk_right', [143,144,145,146,147,148,149,150,151], 150);
 
-    this.renderable.addAnimation('stand', [0,0]);
-    this.renderable.setCurrentAnimation('stand');
+    this.renderable.addAnimation('stand_up', [104,104], 150);
+    this.renderable.addAnimation('stand_down', [130,130], 150);
+    this.renderable.addAnimation('stand_left', [117,117], 150);
+    this.renderable.addAnimation('stand_right', [143,143], 150);
+
+    this.direction = 'up';
+
+    this.renderable.setCurrentAnimation('stand_up');
 
     this.renderable.anchorPoint = new me.Vector2d(0.5,0.8);
     this.body.addShape(
@@ -41,29 +47,33 @@ export default class MainPlayer extends me.ComposableSpritePlugin.ComposableSpri
   {
     if (me.input.isKeyPressed('left')) {
       this.body.vel.x = -VELOCITY;
+      this.direction = 'left';
       if (!this.renderable.isCurrentAnimation('walk_left')) {
         this.renderable.setCurrentAnimation('walk_left');
       }
     } else if (me.input.isKeyPressed('right')) {
       this.body.vel.x = VELOCITY;
+      this.direction = 'right';
       if (!this.renderable.isCurrentAnimation('walk_right')) {
         this.renderable.setCurrentAnimation('walk_right');
       }
     } else if (me.input.isKeyPressed('up')) {
       this.body.vel.y = -VELOCITY;
+      this.direction = 'up';
       if (!this.renderable.isCurrentAnimation('walk_up')) {
         this.renderable.setCurrentAnimation('walk_up');
       }
     } else if (me.input.isKeyPressed('down')) {
       this.body.vel.y = VELOCITY;
+      this.direction = 'down';
       if (!this.renderable.isCurrentAnimation('walk_down')) {
         this.renderable.setCurrentAnimation('walk_down');
       }
     } else {
       this.body.vel.x = 0;
       this.body.vel.y = 0;
-      if (!this.renderable.isCurrentAnimation('stand')) {
-        this.renderable.setCurrentAnimation('stand');
+      if (!this.renderable.isCurrentAnimation(`stand_${this.direction}`)) {
+        this.renderable.setCurrentAnimation(`stand_${this.direction}`);
       }
     }
     this.body.update(time);
